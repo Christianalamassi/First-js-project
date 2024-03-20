18c5cdf08784d2e5db192ff1f9dfab3ece258ec8
Ahmed Alamassi (Christian) <christianalamassi@gmail.com>
2024-03-20 16:36:51 +0000

    Working on the score fun

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,45 @@
+//The Html Elements I called
 let photos1 = document.getElementById('photo1');
 let photos2 = document.getElementById('photo2');
 
+
 let dicesA = document.getElementById('diceA');
-let dicesB = document.getElementById('diceB');
+let dicesB = document.getElementById("diceB");
 
-let scoreArea1 = document.getElementById('score-area1');
-let scoreArea2 = document.getElementById('score-area2');
 
-let buttonsA = document.getElementById('buttonA');
-let buttonsB = document.getElementById('buttonB');
+let scoreArea1 = document.getElementById('score-area1').innerText;
+let scoreArea2 = document.getElementById('score-area2').innerText;
+
+let buttons = document.getElementsByTagName('button');
 
 let wins = document.getElementById('win');
 
 
+// the workarea
+document.addEventListener('click', function () {
+    let fafb = faceDiceA() + " " + "," + " " + faceDiceB();
+    let fcfd = faceDiceC() + " " + "," + " " + faceDiceD();
+
+    if (fafb >= fcfd) {
+        document.getElementById('score-area1').innerText = ++scoreArea1;
+    } else {
+        document.getElementById('score-area2').innerText = ++scoreArea2;
+    }
+
+
+    photos1.src = `../iamges/d-${faceDiceA()}.jpg` + `../iamges/d-${faceDiceB()}.jpg`;
+    photos2.src = `../iamges/d-${faceDiceC()}.jpg` + `../iamges/d-${faceDiceD()}.jpg`;
+
+
+    dicesA.innerText = fafb;
+    dicesB.innerText = fcfd;
+    console.log(scoreNum());
+
+});
+
 function faceDiceA() {
     return Math.ceil(Math.random() * 6);
+
 }
 
 function faceDiceB() {
@@ -22,13 +47,11 @@ function faceDiceB() {
 
 }
 
+function faceDiceC() {
+    return Math.ceil(Math.random() * 6);
+}
 
-buttonsA.addEventListener('click', function () {
-    let fafb = faceDiceA() + ' ' + ',' + ' ' + faceDiceB();
-    dicesA.innerText = fafb;
-});
+function faceDiceD() {
+    return Math.ceil(Math.random() * 6);
 
-buttonsB.addEventListener('click', function () {
-    let fafb = faceDiceA() + ' ' + ',' + ' ' + faceDiceB();
-    dicesB.innerText = fafb;
-});
+}
