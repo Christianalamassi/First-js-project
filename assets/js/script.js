1704236f5a90bdcd0d8c8ec6bfab666c98f2021b
Ahmed Alamassi (Christian) <christianalamassi@gmail.com>
2024-03-20 08:51:02 +0000

    Add HTML codes

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+function rolldice():
+    import random
+    roll = random.randint(1,6)
+    console.log ("you got", roll)
+  
+console.log ("\033[34m","Hello there")
+q= input ("would you like to roll the Dice?")
+if (q == "y");
+    console.log ("lets roll", "\U0001f600")
+ 
+  
+    while True:
+        rolldice()
+       
+        qq=input ("would you like to play more?")
+        if (qq== "y"){
+            continue}
+        else:{
+            exit()}
