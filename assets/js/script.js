289aefb25596a35ac776da358cafe9975f04788c
Ahmed Alamassi (Christian) <christianalamassi@gmail.com>
2024-03-20 11:09:25 +0000

    Add the elements I will work on them

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+let photos = document.getElementsByClassName('photo');
+let dices = document.getElementsByClassName('dics');
+let scoreArea = document.getElementsByClassName('score-area');
+let buttons = document.getElementsByTagName('button');
+let wins = document.getElementById('win');
+
+buttons = document.addEventListener('click'function ());
