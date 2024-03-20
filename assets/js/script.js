4078dfac41c845afd8bd397fd2d8a4f01b42f854
Ahmed Alamassi (Christian) <christianalamassi@gmail.com>
2024-03-20 13:55:24 +0000

    Fix bug in the button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,34 @@
-let photos = document.getElementsByClassName('photo');
-let dices = document.getElementsByClassName('dics');
-let scoreArea = document.getElementsByClassName('score-area');
-let buttons = document.getElementsByTagName('button');
+let photos1 = document.getElementById('photo1');
+let photos2 = document.getElementById('photo2');
+
+let dicesA = document.getElementById('diceA');
+let dicesB = document.getElementById('diceB');
+
+let scoreArea1 = document.getElementById('score-area1');
+let scoreArea2 = document.getElementById('score-area2');
+
+let buttonsA = document.getElementById('buttonA');
+let buttonsB = document.getElementById('buttonB');
+
 let wins = document.getElementById('win');
 
-buttons = document.addEventListener('click'function ());
+
+function faceDiceA() {
+    return Math.ceil(Math.random() * 6);
+}
+
+function faceDiceB() {
+    return Math.ceil(Math.random() * 6);
+
+}
+
+
+buttonsA.addEventListener('click', function () {
+    let fafb = faceDiceA() + ' ' + ',' + ' ' + faceDiceB();
+    dicesA.innerText = fafb;
+});
+
+buttonsB.addEventListener('click', function () {
+    let fafb = faceDiceA() + ' ' + ',' + ' ' + faceDiceB();
+    dicesB.innerText = fafb;
+});
